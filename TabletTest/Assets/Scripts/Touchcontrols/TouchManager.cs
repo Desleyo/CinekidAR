97d3bed15b8b0758da4d8f5b8fdd5023b4f60acd
Desleyo <desleyoord@gmail.com>
2022-06-13 15:10:06 +0200

    swipe down control for hooks

--- TabletTest/Assets/Scripts/Touchcontrols/TouchManager.cs
+++ TabletTest/Assets/Scripts/Touchcontrols/TouchManager.cs
@@ -7,41 +7,55 @@ public class TouchManager : MonoBehaviour
 {
     [SerializeField] Text debugText;
 
+    [SerializeField] float swipeRange = 50f;
     float startPosY, currentPosY;
+    bool swipingDown;
+
+    Interactable currentInteractable;
 
     private void Update()
     {
         if (Input.touchCount == 0)
             return;
 
-        CheckTouchInput();
-        CheckIfSwipingDown();
+        CheckTouchBegan();
+        CheckTouchEnded();
     }
 
-    void CheckTouchInput()
+    void CheckTouchBegan()
     {
         if (Input.GetTouch(0).phase != TouchPhase.Began)
             return;
+
+        startPosY = Input.GetTouch(0).position.y;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             if (hit.collider.CompareTag("Interactable"))
             {
-                hit.collider.GetComponent<Interactable>().HookTriggered(false);
+                currentInteractable = hit.collider.GetComponentInParent<Interactable>();
             }
         }
     }
 
-    void CheckIfSwipingDown()
+    void CheckTouchEnded()
     {
-        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.GetTouch(0).phase != TouchPhase.Ended)
+            return;
+
+        if (CheckIfSwipingDown())
         {
-            startPosY = Input.GetTouch(0).position.y;
+            currentInteractable.HookTriggered(false);
         }
-        else if (Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            currentPosY = Input.GetTouch(0).position.y;
-        }
+
+        currentInteractable = null;
+    }
+
+    bool CheckIfSwipingDown()
+    {
+        currentPosY = Input.GetTouch(0).position.y;
+
+        return (currentPosY - startPosY) < -swipeRange;
     }
 }
